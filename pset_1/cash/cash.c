ba5bebf554c7739a82c6625419d35d4f87aa085d
kellytung2027 <144371984+kellytung2027@users.noreply.github.com>
2023-09-13 23:55:34 +0000

    Wed, Sep 13, 2023, 7:55 PM -04:00

--- pset_1/cash/cash.c
+++ pset_1/cash/cash.c
@@ -17,25 +17,25 @@ int main(void)
     // checking for quarters
     if (change >= 25)
     {
-        int num_quarters = change/25;
+        int num_quarters = change / 25;
         coins += num_quarters;
-        change -= num_quarters*25;
+        change -= num_quarters * 25;
     }
 
     // checking for dimes
     if (change >= 10)
     {
-        int num_dimes = change/10;
+        int num_dimes = change / 10;
         coins += num_dimes;
-        change -= num_dimes*10;
+        change -= num_dimes * 10;
     }
 
     // checking for nickels
     if (change >= 5)
     {
-        int num_nickels = change/5;
+        int num_nickels = change / 5;
         coins += num_nickels;
-        change -= num_nickels*5;
+        change -= num_nickels * 5;
     }
 
     // adding remaining change as pennies
