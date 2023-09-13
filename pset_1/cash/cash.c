55ba719de04ebd589d0acab3566a1735aad1168b
kellytung2027 <144371984+kellytung2027@users.noreply.github.com>
2023-09-13 23:47:11 +0000

    Wed, Sep 13, 2023, 7:47 PM -04:00

--- pset_1/cash/cash.c
+++ pset_1/cash/cash.c
@@ -15,6 +15,24 @@ int main(void)
     int coins = 0;
     while (change != 0)
     {
-        if (coins%25)
+        if (change >= 25)
+        {
+            int num_quarters = change/25;
+            coins += num_quarters;
+            change -= num_quarters*25;
+        }
+        if (change >= 10)
+        {
+            int num_dimes = change/10;
+            coins += num_dimes;
+            change -= num_dimes*10;
+        }
+        if (change >= 5)
+        {
+            int num_nickels = change/5;
+            coins += num_nickels;
+            change -= num_nickels*5;
+        }
+        
     }
 }
