93a5337a1a494e1dbe8918db2a07ad7a2079c9af
kellytung2027 <144371984+kellytung2027@users.noreply.github.com>
2023-09-13 19:44:23 +0000

    Wed, Sep 13, 2023, 3:44 PM -04:00

--- exercise.c
+++ exercise.c
@@ -5,7 +5,9 @@ int main(void)
 {
     string name = get_string("What is your name? ");
     int age = get_int("What is your age? ");
+    // could store as string and float
     long phone_number = get_long("What is your phone number? ");
+    // could store as string or double too
 
     printf("hello, %s, your age is %i and your phone number is %ld\n", name, age, phone_number);
 }
