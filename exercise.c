925d3ef2173c645e97c32e77f1e0dab4f5a901d5
kellytung2027 <144371984+kellytung2027@users.noreply.github.com>
2023-09-13 19:38:50 +0000

    Wed, Sep 13, 2023, 3:38 PM -04:00

--- exercise.c
+++ exercise.c
@@ -5,8 +5,8 @@ int main(void)
 {
     string name = get_string("What is your name? ");
     int age = get_int("What is your age? ");
-    float phone_number = get_int("What is your phone number? ");
+     phone_number = get_int("What is your phone number? ");
 
-    printf("hello, %s, your age is %i and your phone nuber is %i", name, age, phone_number);
+    printf("hello, %s, your age is %i and your phone number is %i", name, age, phone_number);
 
 }
