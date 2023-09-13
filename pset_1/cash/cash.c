dd329ce26bc440916be2c6a0f0a73993f6c42154
kellytung2027 <144371984+kellytung2027@users.noreply.github.com>
2023-09-13 23:50:17 +0000

    Wed, Sep 13, 2023, 7:50 PM -04:00

--- pset_1/cash/cash.c
+++ pset_1/cash/cash.c
@@ -13,26 +13,32 @@ int main(void)
 
     // counting the minimum # of coins
     int coins = 0;
-    while (change != 0)
+
+    // checking for quarters
+    if (change >= 25)
     {
-        if (change >= 25)
-        {
-            int num_quarters = change/25;
-            coins += num_quarters;
-            change -= num_quarters*25;
-        }
-        if (change >= 10)
-        {
-            int num_dimes = change/10;
-            coins += num_dimes;
-            change -= num_dimes*10;
-        }
-        if (change >= 5)
-        {
-            int num_nickels = change/5;
-            coins += num_nickels;
-            change -= num_nickels*5;
-        }
-        
+        int num_quarters = change/25;
+        coins += num_quarters;
+        change -= num_quarters*25;
     }
+
+    // checking for dimes
+    if (change >= 10)
+    {
+        int num_dimes = change/10;
+        coins += num_dimes;
+        change -= num_dimes*10;
+    }
+
+    // checking for nickels
+    if (change >= 5)
+    {
+        int num_nickels = change/5;
+        coins += num_nickels;
+        change -= num_nickels*5;
+    }
+
+    // adding remaining change as pennies
+    coins += change;
+    printf("%i", coins)
 }
