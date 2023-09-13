c9b9bf818c22e9cefa31af6ed2a4c656a7ffb3d2
kellytung2027 <144371984+kellytung2027@users.noreply.github.com>
2023-09-13 23:38:23 +0000

    Wed, Sep 13, 2023, 7:38 PM -04:00

--- pset_1/cash/cash.c
+++ pset_1/cash/cash.c
@@ -3,5 +3,18 @@
 
 int main(void)
 {
-    
+    // getting user input
+    int change;
+    do
+    {
+        change = get_int("Change owed: ");
+    }
+    while (height <= 0);
+
+    // counting the minimum # of coins
+    int coins = 0;
+    while (change != 0)
+    {
+        if (coins%25)
+    }
 }
