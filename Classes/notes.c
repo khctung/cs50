ce6f8e60d0877f948fb3d58a30f8b668a582745c
kellytung2027 <144371984+kellytung2027@users.noreply.github.com>
2023-09-19 21:23:44 +0000

    Tue, Sep 19, 2023, 5:23 PM -04:00

--- Classes/notes.c
+++ Classes/notes.c
@@ -22,6 +22,12 @@ clang -o hello hello.c  // cmomputer runs compiler and stores as hello
 ./hello                 // runs hello
 clang -o hello hello.c -lcs50 // l = library you're using, then the name of it
 
+// converting source code to machine code
+// 1. pre-processing -> using #include to call prototypes for functions
+// 2. compiling -> converting coded language to assembly language
+// 3. assembling -> converts assmembly language to binary
+// 4. linking -> 
+
 int main(void) // putting main code in, returns 0
 {
 
