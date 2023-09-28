c5e8f9696f09d11bdbe96b7b3d592f575804b693
kellytung2027 <144371984+kellytung2027@users.noreply.github.com>
2023-09-28 06:56:08 +0000

    Thu, Sep 28, 2023, 2:56 AM -04:00

--- pset_3/runoff/runoff.c
+++ pset_3/runoff/runoff.c
@@ -141,7 +141,10 @@ bool vote(int voter, int rank, string name)
 // Tabulate votes for non-eliminated candidates
 void tabulate(void)
 {
-    
+    for (int i = 0; i < voter_count; i++)
+    {
+        
+    }
     return;
 }
 
