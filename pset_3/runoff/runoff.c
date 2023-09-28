c65d0f1a39c8566b7365e4d89e00893a70e068b3
kellytung2027 <144371984+kellytung2027@users.noreply.github.com>
2023-09-28 07:05:42 +0000

    Thu, Sep 28, 2023, 3:05 AM -04:00

--- pset_3/runoff/runoff.c
+++ pset_3/runoff/runoff.c
@@ -158,7 +158,10 @@ void tabulate(void)
 // Print the winner of the election, if there is one
 bool print_winner(void)
 {
-    for (int i = 0; i < candidates)
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if (candidates.votes >= voter.count/2)
+    }
     return false;
 }
 
