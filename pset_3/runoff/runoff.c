113e6f65a41286419a436f23817d986f08b5f591
kellytung2027 <144371984+kellytung2027@users.noreply.github.com>
2023-09-28 06:53:48 +0000

    Thu, Sep 28, 2023, 2:53 AM -04:00

--- pset_3/runoff/runoff.c
+++ pset_3/runoff/runoff.c
@@ -131,7 +131,7 @@ bool vote(int voter, int rank, string name)
     {
         if (strcmp(name, candidates[i].name) == 0)
         {
-            candidates[voter][].votes++;
+            preferences[voter][rank] = i;
             return true;
         }
     }
