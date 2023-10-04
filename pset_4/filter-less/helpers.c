120ee754a62dc34f05ffdf6f95fec928932b2527
kellytung2027 <144371984+kellytung2027@users.noreply.github.com>
2023-10-04 23:19:09 +0000

    Wed, Oct 4, 2023, 7:19 PM -04:00

--- pset_4/filter-less/helpers.c
+++ pset_4/filter-less/helpers.c
@@ -71,5 +71,14 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
+    // Create a copy of image
+    RGBTRIPLE copy[height][width];
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            copy[i][j] = image[i][j];
+        }
+    }
     return;
 }
