85df039d86c17dc04de72a70b0aa96e6e691c6e8
kellytung2027 <144371984+kellytung2027@users.noreply.github.com>
2023-10-04 23:17:21 +0000

    Wed, Oct 4, 2023, 7:17 PM -04:00

--- pset_4/filter-less/helpers.c
+++ pset_4/filter-less/helpers.c
@@ -60,9 +60,9 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
         for (int j = 0; j < width/2; j++)
         {
             // Swap pixels
-            RGBTRIPLE tmp = *image[i][j];
-            *image[i][j] = *image[i][width - j - 1];
-            *image[i][width - j - 1] = tmp;
+            RGBTRIPLE tmp = image[i][j];
+            image[i][j] = image[i][width - j - 1];
+            image[i][width - j - 1] = tmp;
         }
     }
     return;
