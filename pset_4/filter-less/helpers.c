d2b38f07202dd9a87d81047440d694abcc05ed76
kellytung2027 <144371984+kellytung2027@users.noreply.github.com>
2023-10-04 21:13:06 +0000

    Wed, Oct 4, 2023, 5:13 PM -04:00

---
+++ pset_4/filter-less/helpers.c
@@ -0,0 +1,39 @@
+#include "helpers.h"
+
+// Convert image to grayscale
+void grayscale(int height, int width, RGBTRIPLE image[height][width])
+{
+    // Loop over all pixels
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            // Take average of red, green, and blue
+
+            // Update pixel values
+        }
+    }
+    return;
+}
+
+// Convert image to sepia
+void sepia(int height, int width, RGBTRIPLE image[height][width])
+{
+    return;
+
+    bound sepia
+}
+
+// Reflect image horizontally
+void reflect(int height, int width, RGBTRIPLE image[height][width])
+{
+    return;
+
+    temp needed
+}
+
+// Blur image
+void blur(int height, int width, RGBTRIPLE image[height][width])
+{
+    return;
+}
