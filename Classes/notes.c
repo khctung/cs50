e7291400020a0cdabea4b633bab8466c8916d068
kellytung2027 <144371984+kellytung2027@users.noreply.github.com>
2023-10-04 19:56:32 +0000

    Wed, Oct 4, 2023, 3:56 PM -04:00

--- Classes/notes.c
+++ Classes/notes.c
@@ -185,6 +185,24 @@ void fourth_week(int lazy)  // creating your own method/function
     buffer = used to store information
 
     BMP = bit map to store bytes as a map/grid (for images)
+
+    typedef uin8_t BYTE;
+
+    int main(int argc, char *argv[])
+    {
+        FILE *src = fopen(argv[1], "rb");
+        FILE *dst = fopen(argv[2], "wb");
+
+        BYTE b;
+
+        while (fread(&b, sizeof(b), 1, src) != 0)
+        {
+            fwrite(&b, sizeof(b), 1, dst);
+        }
+
+        fclose(dst);
+        fclose(src);
+    }
 }
 
 void swap(int *a, int *b)
