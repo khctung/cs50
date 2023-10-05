27f46861f1abe05151a0b873205bcc3a3f990a5b
kellytung2027 <144371984+kellytung2027@users.noreply.github.com>
2023-10-05 05:31:37 +0000

    Thu, Oct 5, 2023, 1:31 AM -04:00

--- pset_4/recover/recover.c
+++ pset_4/recover/recover.c
@@ -28,7 +28,7 @@ int main(int argc, char *argv[])
     {
         // Create JPEGs from the data
 
-        jpeg_count++;
+        // ignore jpeg_count++;
 
 
         if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && buffer[3] >= 0xe0 && buffer[3] <= 0xef)
