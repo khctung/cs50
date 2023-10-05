ed5ce4b55ea7d7f07030655872c23cccd69d80e1
kellytung2027 <144371984+kellytung2027@users.noreply.github.com>
2023-10-05 05:02:55 +0000

    Thu, Oct 5, 2023, 1:02 AM -04:00

--- pset_4/recover/recover.c
+++ pset_4/recover/recover.c
@@ -1,7 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
- 
+
 int main(int argc, char *argv[])
 {
- 
+    // Accept a single command-line argument
+    if (argc != 2)
+    {
+        printf("Usage: ./recover FILE\n");
+        return 1;
+    }
+
+    // Open the memory card
+    FILE *card = fopen(argv[1], "r");
+
+    // CHECK IF OPENED PROPERLY
+
+    // Create a buffer for a block of data
+    uint8_t buffer[512];
+
+    // While there's still data left to read from the memory card
+    while (fread(buffer, 1, 512, card) == 512)
+    {
+        // Create JPEGs from the data
+
+    }
+
 }
