2b2ca6c0c32159a7292c6f963f3dc4370e10e04b
kellytung2027 <144371984+kellytung2027@users.noreply.github.com>
2023-10-05 05:18:22 +0000

    Thu, Oct 5, 2023, 1:18 AM -04:00

--- pset_4/recover/recover.c
+++ pset_4/recover/recover.c
@@ -26,7 +26,7 @@ int main(int argc, char *argv[])
     while (fread(buffer, 1, BLOCKSIZE, card) == BLOCKSIZE)
     {
         // Create JPEGs from the data
-        
+        if (buffer[])
     }
 
 }
