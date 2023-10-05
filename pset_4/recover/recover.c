7539f24f043f65af4f04758c3c5da231303f9ecd
kellytung2027 <144371984+kellytung2027@users.noreply.github.com>
2023-10-05 05:30:02 +0000

    Thu, Oct 5, 2023, 1:30 AM -04:00

--- pset_4/recover/recover.c
+++ pset_4/recover/recover.c
@@ -43,7 +43,7 @@ int main(int argc, char *argv[])
             {
                 fclose(inptr);
                 printf("Could not create %s.\n", outfile);
-                return 5;
+                return 3;
             }
         }
     }
