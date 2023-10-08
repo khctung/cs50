c3e9eac56229ab330a1d3c2af473a7b24c27eb22
kellytung2027 <144371984+kellytung2027@users.noreply.github.com>
2023-10-08 18:32:42 +0000

    Sun, Oct 8, 2023, 2:32 PM -04:00

--- tester.c
+++ tester.c
@@ -1,12 +1,16 @@
 #include <cs50.h>
 #include <stdio.h>
 
+void square(int x);
+
 int main(void)
 {
-    int x = 4;
-    int *p1 = &x;
-    int *p2 = p1;
-    *p2 = 3;
-    
-    printf("%d\n", temp);
+    int x = 5;
+    square(*x);
+    printf("%i\n", x);
+}
+
+void square(int *n)
+{
+    *n = &n * &n;
 }
