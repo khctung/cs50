f4a7f07cc232092cc41d4a72caa4994d92c54736
kellytung2027 <144371984+kellytung2027@users.noreply.github.com>
2023-10-08 19:44:59 +0000

    Sun, Oct 8, 2023, 3:44 PM -04:00

--- pset_4/filter-less/helpers.c
+++ pset_4/filter-less/helpers.c
@@ -103,18 +103,14 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
             int sum_blue = 0;
 
             // sifting through nearby pixels (touching current pixel)
-            for (int k = max(i - 1, 0); k <= i + 1; k++)
+            for (int k = max(i - 1, 0); k <= min(i + 1, height - 1); k++)
             {
-                for (int l = max(j - 1); l <= j + 1; l++)
+                for (int l = max(j - 1, 0); l <= min(j + 1, width - 1); l++)
                 {
-                    // checking if valid (in image)
-                    if (min(0, k) == 0 && max(k, height - 1) == height - 1 && min(0, l) == 0 && max(l, width - 1) == width - 1)
-                    {
-                        num_pixels++;
-                        sum_red += copy[k][l].rgbtRed;
-                        sum_green += copy[k][l].rgbtGreen;
-                        sum_blue += copy[k][l].rgbtBlue;
-                    }
+                    num_pixels++;
+                    sum_red += copy[k][l].rgbtRed;
+                    sum_green += copy[k][l].rgbtGreen;
+                    sum_blue += copy[k][l].rgbtBlue;
                 }
             }
 
