4fce7cd302ff8bb28b71e48d8be0e36bec7a227e
kellytung2027 <144371984+kellytung2027@users.noreply.github.com>
2023-10-16 07:16:31 +0000

    Mon, Oct 16, 2023, 3:16 AM -04:00

--- pset_5/speller/dictionary.c
+++ pset_5/speller/dictionary.c
@@ -35,6 +35,7 @@ bool check(const char *word)
 // Hashes word to a number
 unsigned int hash(const char *word)
 {
+    
     // TODO: Improve this hash function
     return toupper(word[0]) - 'A';
 }
