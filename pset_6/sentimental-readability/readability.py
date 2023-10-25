03ae735857f49c21282eeec5e6aff1cea656ca16
kellytung2027 <144371984+kellytung2027@users.noreply.github.com>
2023-10-25 23:33:56 +0000

    Wed, Oct 25, 2023, 7:33 PM -04:00

--- pset_6/sentimental-readability/readability.py
+++ pset_6/sentimental-readability/readability.py
@@ -1,5 +1,6 @@
 import cs50
 
+
 def main():
     text = input("Text: ")
 
@@ -7,13 +8,13 @@ def main():
     letters = get_letters(text)
     # calculating number of words
     words = get_words(text)
-    #calculating number of sentences
+    # calculating number of sentences
     sentences = get_sentences(text)
 
     # computing grade level
     L = (letters / words) * 100
     S = (sentences / words) * 100
-    grade_level = round(0.0588 * L - 0.296 * S - 15.8);
+    grade_level = round(0.0588 * L - 0.296 * S - 15.8)
 
     # deciding what to print based on grade level
     if grade_level < 1:
@@ -23,29 +24,33 @@ def main():
     else:
         print(f"Grade {int(grade_level)}")
 
+
 # function for getting the # of letters
 def get_letters(input):
     num_letters = 0
     for i in range(len(input)):
-        if (input[i].isalpha()):
+        if input[i].isalpha():
             num_letters += 1
     return num_letters
+
 
 # function for getting the # of words
 def get_words(input):
     num_words = 0
     for i in range(len(input)):
-        if (input[i].isspace()):
+        if input[i].isspace():
             num_words += 1
     num_words += 1
     return num_words
+
 
 # function for getting the # of sentences
 def get_sentences(input):
     num_sentences = 0
     for i in range(len(input)):
-        if (input[i] == '!' or input[i] == '.' or input[i] == '?'):
-            num_sentences += 1;
+        if input[i] == "!" or input[i] == "." or input[i] == "?":
+            num_sentences += 1
     return num_sentences
+
 
 main()
