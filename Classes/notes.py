485bde0f20b1fb56f10ed9e47ce829520d0351e7
kellytung2027 <144371984+kellytung2027@users.noreply.github.com>
2023-10-25 19:26:00 +0000

    Wed, Oct 25, 2023, 3:26 PM -04:00

--- Classes/notes.py
+++ Classes/notes.py
@@ -1,5 +1,5 @@
-
-
 code hello.py
 python hello.py
-// doesn't need separate compiler, just functions as interpreter
+# doesn't need separate compiler, just functions as interpreter
+
+# python allocates memory for you, but might be slower than c because of this
