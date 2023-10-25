ff298da614b120b192aa2d8be6b554d7346df296
kellytung2027 <144371984+kellytung2027@users.noreply.github.com>
2023-10-25 23:11:48 +0000

    Wed, Oct 25, 2023, 7:11 PM -04:00

--- pset_6/sentimental-cash/cash.py
+++ pset_6/sentimental-cash/cash.py
@@ -11,17 +11,21 @@ change *= 100
 
 coins = 0
 
+# calculating change for quarters
 num_quarters = change // 25
 coins += num_quarters
 change -= num_quarters * 25
 
+# calculating change for dimes
 num_dimes = change // 10
 coins += num_dimes
 change -= num_dimes * 10
 
+# calculating change for nickels
 num_nickels = change // 5
 coins += num_nickels
 change -= num_nickels * 5
 
+# adding directly for pennies
 coins += change
 print(int(coins))
