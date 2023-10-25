6df2d8970fe17ab84f1337e7688ff774c97d2156
kellytung2027 <144371984+kellytung2027@users.noreply.github.com>
2023-10-25 19:35:00 +0000

    Wed, Oct 25, 2023, 3:35 PM -04:00

--- Classes/notes.py
+++ Classes/notes.py
@@ -3,3 +3,7 @@ python hello.py
 # doesn't need separate compiler, just functions as interpreter
 
 # python allocates memory for you, but might be slower than c because of this
+import cs50
+from cs50 import get_string
+
+# don't need to declare type in python
