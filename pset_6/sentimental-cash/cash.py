e9c083840c755bee06541579805f607d37a3a949
kellytung2027 <144371984+kellytung2027@users.noreply.github.com>
2023-10-25 23:07:26 +0000

    Wed, Oct 25, 2023, 7:07 PM -04:00

--- pset_6/sentimental-cash/cash.py
+++ pset_6/sentimental-cash/cash.py
@@ -6,17 +6,20 @@ while True:
     if change > 0:
         break
 
+# converting dollar to cents
+change *= 100
+
 coins = 0
 
-num_quarters = (change/25).floor()
+num_quarters = change // 25
 coins += num_quarters
 change -= num_quarters*25
 
-num_dimes = (change/10).floor()
+num_dimes = change // 10
 coins += num_dimes
 change -= num_dimes*10
 
-num_nickels = (change/5).floor()
+num_nickels = change // 5
 coins += num_nickels
 change -= num_nickels*5
 
