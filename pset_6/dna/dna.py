ff4763b898e774a97dd852b96c736efef428d9b5
kellytung2027 <144371984+kellytung2027@users.noreply.github.com>
2023-10-29 20:36:15 +0000

    Sun, Oct 29, 2023, 4:36 PM -04:00

--- pset_6/dna/dna.py
+++ pset_6/dna/dna.py
@@ -13,6 +13,7 @@ def main():
         reader = csv.DictReader(file)
         for row in reader:
             database.append(row)
+    print(database)
 
     # Read DNA sequence file into a variable
     with open(sys.argv[2], "r") as file:
