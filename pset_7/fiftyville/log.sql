c9fe8f4c918aebcae39f6ecc323e19117d68592a
kellytung2027 <144371984+kellytung2027@users.noreply.github.com>
2023-11-02 16:21:39 +0000

    Thu, Nov 2, 2023, 12:21 PM -04:00

--- pset_7/fiftyville/log.sql
+++ pset_7/fiftyville/log.sql
@@ -34,6 +34,12 @@ VALUES (
     WHERE month = 7 AND day = 28 AND hour = 10 AND (minute >= 5 OR minute <= 25)
 );
 
+INSERT INTO suspects (name)
+SELECT name
+FROM people
+JOIN bakery_security_logs ON bakery_security_logs.license_plate = people.license_plate
+WHERE month = 7 AND day = 28 AND hour = 10 AND (minute >= 5 OR minute <= 25);
+
 
 -- find possible suspects
 SELECT COUNT(name)
