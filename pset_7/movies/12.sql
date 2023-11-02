de3456b6e1d7fcc08c3cb74854a221e3442bdfe1
kellytung2027 <144371984+kellytung2027@users.noreply.github.com>
2023-11-02 08:10:54 +0000

    Thu, Nov 2, 2023, 4:10 AM -04:00

--- pset_7/movies/12.sql
+++ pset_7/movies/12.sql
@@ -1,9 +1,11 @@
 -- write a SQL query to list the titles of all movies in which both Bradley Cooper and Jennifer Lawrence starred.
 SELECT title
 FROM movies
-JOIN stars ON movies.id = stars.movie_id
-JOIN people ON stars.person_id = people.id
-WHERE people.name = 'Bradley Cooper' AND people.name = 'Jennifer Lawrence';
+WHERE id IN (
+  SELECT movie_id FROM stars JOIN people ON stars.person_id = people.id WHERE name = 'Bradley Cooper'
+) AND id IN (
+  SELECT movie_id FROM stars JOIN people ON stars.person_id = people.id WHERE name = 'Jennifer Lawrence'
+);
 
 
 
