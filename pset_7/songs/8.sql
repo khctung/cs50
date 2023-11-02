9e104700dcf3c8cea492111deeeeb8b73ddf7900
kellytung2027 <144371984+kellytung2027@users.noreply.github.com>
2023-11-02 05:51:05 +0000

    Thu, Nov 2, 2023, 1:51 AM -04:00

--- pset_7/songs/8.sql
+++ pset_7/songs/8.sql
@@ -1,6 +1,4 @@
-SELECT names FROM songs WHERE
-name
-
-write a SQL query that lists the names of the songs that feature other artists.
-Songs that feature other artists will include “feat.” in the name of the song.
-Your query should output a table with a single column for the name of each song.
+-- The names of songs that feature other artists.
+SELECT name
+FROM songs
+WHERE name LIKE '%feat.%';
