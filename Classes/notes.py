e6cf9d72c40c5fd925d71e5be6ab75b65065c84d
kellytung2027 <144371984+kellytung2027@users.noreply.github.com>
2023-11-02 03:56:30 +0000

    Wed, Nov 1, 2023, 11:56 PM -04:00

--- Classes/notes.py
+++ Classes/notes.py
@@ -177,6 +177,7 @@ FOREIGN KEY = in key but not created in them, primary keys in another table
 
 one-to-one = every x has one y
 one-to-many = every x can have multiple y
+many-to-many = multiple x can have multiple y
 can have nested queries -> put one inside the other
 
 SELECT * shows WHERE id IN (SELECT show_id FROM ratings WHERE rating >= 6.0) # nested query
