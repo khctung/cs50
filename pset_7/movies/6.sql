9cecba7a1f4ebde76d80878509ee40f75f089da2
kellytung2027 <144371984+kellytung2027@users.noreply.github.com>
2023-11-02 06:50:24 +0000

    Thu, Nov 2, 2023, 2:50 AM -04:00

--- pset_7/movies/6.sql
+++ pset_7/movies/6.sql
@@ -1,7 +1,12 @@
 -- write a SQL query to determine the average rating of all movies released in 2012.
 SELECT AVG(rating)
 from ratings
-WHERE ()
+WHERE movie_id
+IN (SELECT id FROM movies WHERE year = 2012);
+
+SELECT title FROM shows WHERE id IN
+ (SELECT show_id FROM stars WHERE person_id =
+  (SELECT id FROM people WHERE name = 'Steve Carell'));
 
 
 Your query should output a table with a single column and a single row (not counting the header) containing the average rating.
