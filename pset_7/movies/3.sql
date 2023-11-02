8a20b6cbc7c65e3c0a553292816bf8e83c80e450
kellytung2027 <144371984+kellytung2027@users.noreply.github.com>
2023-11-02 06:19:29 +0000

    Thu, Nov 2, 2023, 2:19 AM -04:00

--- pset_7/movies/3.sql
+++ pset_7/movies/3.sql
@@ -1,5 +1,5 @@
 -- write a SQL query to list the titles of all movies with a release date on or after 2018, in alphabetical order
-SELECT title FROM movies WHERE year >= 2018 ORDER BY 
+SELECT COUNT(title) FROM movies WHERE year >= 2018 ORDER BY title ASC;
 
 
 Your query should output a table with a single column for the title of each movie.
