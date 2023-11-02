c1cf986005c68a02027dfffadba59af8ed0c7976
kellytung2027 <144371984+kellytung2027@users.noreply.github.com>
2023-11-02 08:15:53 +0000

    Thu, Nov 2, 2023, 4:15 AM -04:00

--- pset_7/movies/12.sql
+++ pset_7/movies/12.sql
@@ -4,13 +4,11 @@ FROM movies
 WHERE id IN (
   SELECT movie_id
   FROM stars
-  JOIN people
-  ON stars.person_id = people.id
+  JOIN people ON stars.person_id = people.id
   WHERE name = 'Bradley Cooper'
 ) AND id IN (
   SELECT movie_id
   FROM stars
-  JOIN people
-  ON stars.person_id = people.id
+  JOIN people ON stars.person_id = people.id
   WHERE name = 'Jennifer Lawrence'
 );
