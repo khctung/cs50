179858259c69ee5611b00ec0b804bc1a3e80cfba
kellytung2027 <144371984+kellytung2027@users.noreply.github.com>
2023-11-02 07:05:31 +0000

    Thu, Nov 2, 2023, 3:05 AM -04:00

--- pset_7/movies/8.sql
+++ pset_7/movies/8.sql
@@ -4,6 +4,3 @@ FROM people
 JOIN stars ON people.id = stars.person_id
 JOIN movies ON stars.movie_id = movies.id
 WHERE movies.title = 'Toy Story';
-
-Your query should output a table with a single column for the name of each person.
-You may assume that there is only one movie in the database with the title Toy Story.
