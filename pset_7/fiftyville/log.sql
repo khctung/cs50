9a40db4492ec6406ad7eb1525a330a6a79ec54a5
kellytung2027 <144371984+kellytung2027@users.noreply.github.com>
2023-11-02 16:39:44 +0000

    Thu, Nov 2, 2023, 12:39 PM -04:00

--- pset_7/fiftyville/log.sql
+++ pset_7/fiftyville/log.sql
@@ -11,12 +11,12 @@ SELECT transcript
 FROM interviews
 WHERE year = 2023 AND month = 7 AND day = 28 AND transcript LIKE "%bakery%";
 -- info given:
--- (1) +/- 10 mins of theft, got into car in bakery parking lot
+-- (1) within 10 mins of theft, got into car in bakery parking lot
 -- (2) before theft (morning), saw thief withdrawing money from ATM on Leggett Street
 -- (3) thief leaving bakery -> called someone who talked < 1 min. thief plans to take earliest flight out of fiftyville tomorrow, person on phone purchased ticket
 
 -- storing current suspects into a table
--- suspects generated via checking (1) from transcript: bakery security log transcripts +/- 10 mins of theft
+-- suspects generated via checking (1) from transcript: bakery security log transcripts within 10 mins of theft
 CREATE TABLE suspects (
   name TEXT NOT NULL
 );
@@ -25,7 +25,7 @@ VALUES (
     SELECT name
     FROM people
     JOIN bakery_security_logs ON bakery_security_logs.license_plate = people.license_plate
-    WHERE month = 7 AND day = 28 AND hour = 10 AND (minute >= 5 OR minute <= 25)
+    WHERE bakery_security_logs.month = 7 AND day = 28 AND hour = 10 AND (minute >= 15 OR minute <= 25)
 );
 -- current list of suspects: Brandon, Sophia, Vanessa, Bruce, Barry, Luca, Sofia, Iman, Diana, Kelsey, Taylor, Denise, Thomas, Jeremy
 
