1e4c615a7d916b6136b1407d03129a6fe24c524a
kellytung2027 <144371984+kellytung2027@users.noreply.github.com>
2023-11-02 16:23:20 +0000

    Thu, Nov 2, 2023, 12:23 PM -04:00

--- pset_7/fiftyville/log.sql
+++ pset_7/fiftyville/log.sql
@@ -15,14 +15,8 @@ WHERE month = 7 AND day = 28;
 -- (2) before theft (morning), saw thief withdrawing money from ATM on Leggett Street
 -- (3) thief leaving bakery -> called someone who talked < 1 min. thief plans to take earliest flight out of fiftyville tomorrow, person on phone purchased ticket
 
--- check bakery security log transcripts +/- 10 mins of theft
-SELECT name
-FROM people
-JOIN bakery_security_logs ON bakery_security_logs.license_plate = people.license_plate
-WHERE month = 7 AND day = 28 AND hour = 10 AND (minute >= 5 OR minute <= 25);
--- list of suspects: Brandon, Sophia, Vanessa, Bruce, Barry, Luca, Sofia, Iman, Diana, Kelsey, Taylor, Denise, Thomas, Jeremy
-
 -- storing current suspects into a table
+-- suspects generated via checking bakery security log transcripts +/- 10 mins of theft
 CREATE TABLE suspects (
   name TEXT NOT NULL
 );
@@ -33,6 +27,7 @@ VALUES (
     JOIN bakery_security_logs ON bakery_security_logs.license_plate = people.license_plate
     WHERE month = 7 AND day = 28 AND hour = 10 AND (minute >= 5 OR minute <= 25)
 );
+-- current list of suspects: Brandon, Sophia, Vanessa, Bruce, Barry, Luca, Sofia, Iman, Diana, Kelsey, Taylor, Denise, Thomas, Jeremy
 
 INSERT INTO suspects (name)
 SELECT name
