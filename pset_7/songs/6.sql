f508d164a6da173aec8cba2ad946745c026dbac5
kellytung2027 <144371984+kellytung2027@users.noreply.github.com>
2023-11-02 05:47:52 +0000

    Thu, Nov 2, 2023, 1:47 AM -04:00

--- pset_7/songs/6.sql
+++ pset_7/songs/6.sql
@@ -1,10 +1,9 @@
 -- The names of songs that are by Post Malone.
-SELECT name FROM songs
-WHERE songs.artist_id = artists.id
-AND name = 'Post Malone';
-
-
-
-write a SQL query that lists the names of songs that are by Post Malone.
-Your query should output a table with a single column for the name of each song.
-You should not make any assumptions about what Post Malone’s artist_id is.
+SELECT name
+FROM songs
+WHERE artist_id =
+(
+    SELECT id
+    FROM artists
+    WHERE name = 'Post Malone'
+);
