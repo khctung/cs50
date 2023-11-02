479f3d07c7cf0a166cb4607af424138815247554
kellytung2027 <144371984+kellytung2027@users.noreply.github.com>
2023-11-02 08:01:27 +0000

    Thu, Nov 2, 2023, 4:01 AM -04:00

---
+++ pset_7/movies/10.sql
@@ -0,0 +1,9 @@
+-- write a SQL query to list the names of all people who have directed a movie that received a rating of at least 9.0.
+SELECT COUNT(DISTINCT name)
+FROM people
+JOIN directors ON people.id = directors.person_id
+JOIN movies ON directors.movie_id = movies.id
+WHERE ratings.rating >= 9.0;
+
+Your query should output a table with a single column for the name of each person.
+If a person directed more than one movie that received a rating of at least 9.0, they should only appear in your results once.
