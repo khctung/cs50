9bf0a7d4a6b2622770bd56cd181fa7abc1430fb3
kellytung2027 <144371984+kellytung2027@users.noreply.github.com>
2023-11-02 06:24:10 +0000

    Thu, Nov 2, 2023, 2:24 AM -04:00

---
+++ pset_7/movies/4.sql
@@ -0,0 +1,6 @@
+-- write a SQL query to determine the number of movies with an IMDb rating of 10.0
+SELECT COUNT(*)
+FROM ratings
+WHERE rating = 10.0;
+
+containing the number of movies with a 10.0 rating.
