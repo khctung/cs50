59162bea94f3026cdd92f40e8e092dadad78ffba
kellytung2027 <144371984+kellytung2027@users.noreply.github.com>
2023-11-02 04:49:08 +0000

    Thu, Nov 2, 2023, 12:49 AM -04:00

--- Classes/notes.py
+++ Classes/notes.py
@@ -210,3 +210,17 @@ B-trees = every node has multiple children
 -> trade-off = more memory
 
 primary keys = indexes
+race conditions = order matters when we care 
+
+from cs50 import SQL
+
+db = SQL("sqlite:///favorites.db")
+favorite = input("Favorite: ")
+
+rows = db.execute("SELECT COUNT(*) AS n FROM favorites WHERE problem = ?", favorite)
+row = rows[0]
+
+print(row["n"])
+
+
+
