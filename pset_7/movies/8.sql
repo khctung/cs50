e682c2fd288a120fcd25c4f54d7608f91a5adda0
kellytung2027 <144371984+kellytung2027@users.noreply.github.com>
2023-11-02 07:01:07 +0000

    Thu, Nov 2, 2023, 3:01 AM -04:00

--- pset_7/movies/8.sql
+++ pset_7/movies/8.sql
@@ -1,7 +1,9 @@
 -- write a SQL query to list the names of all people who starred in Toy Story.
 SELECT name
 FROM people
-
+JOIN movies ON ratings.movie_id = movies.id
+WHERE movies.year = 2010
+ORDER BY rating DESC, title ASC;
 
 Your query should output a table with a single column for the name of each person.
 You may assume that there is only one movie in the database with the title Toy Story.
