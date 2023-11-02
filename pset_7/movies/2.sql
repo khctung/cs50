2fb5cbca5324c701ac41593e86a708df488a2e66
kellytung2027 <144371984+kellytung2027@users.noreply.github.com>
2023-11-02 06:14:02 +0000

    Thu, Nov 2, 2023, 2:14 AM -04:00

--- pset_7/movies/2.sql
+++ pset_7/movies/2.sql
@@ -1,2 +1,11 @@
 -- Determine the birth year of Emma Stone
-SELECT birth FROM movies 
+SELECT birth
+FROM movies
+WHERE 
+
+WHERE artist_id =
+(
+    SELECT id
+    FROM artists
+    WHERE name = 'Drake'
+);
