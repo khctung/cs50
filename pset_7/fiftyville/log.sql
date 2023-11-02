28be1a8555866c73f8a53e909da96158b32abbe7
kellytung2027 <144371984+kellytung2027@users.noreply.github.com>
2023-11-02 15:41:41 +0000

    Thu, Nov 2, 2023, 11:41 AM -04:00

--- pset_7/fiftyville/log.sql
+++ pset_7/fiftyville/log.sql
@@ -6,7 +6,8 @@ FROM crime_scene_reports
 WHERE month = 7 AND day = 28 AND street = 'Humphrey Street';
 -- info given: theft took place @ 10:15am at the Humphrey Street bakery, littering took place @ 16:36
 
-
+SELECT transcript
+FROM interviews;
 
 -- find possible suspects
 SELECT COUNT(name)
