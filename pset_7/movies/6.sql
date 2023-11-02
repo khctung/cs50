6949f46f585633c0ee3713f67bf5ed1ec6ce87ae
kellytung2027 <144371984+kellytung2027@users.noreply.github.com>
2023-11-02 07:58:37 +0000

    Thu, Nov 2, 2023, 3:58 AM -04:00

--- pset_7/movies/6.sql
+++ pset_7/movies/6.sql
@@ -6,7 +6,7 @@ IN (SELECT id FROM movies WHERE year = 2012);
 
 -- OR; which is better design?
 
-SELECT AVG(rating)
-FROM ratings
-JOIN movies ON ratings.movie_id = movies.id
-WHERE movies.year = 2012;
+--SELECT AVG(rating)
+--FROM ratings
+--JOIN movies ON ratings.movie_id = movies.id
+--WHERE movies.year = 2012;
