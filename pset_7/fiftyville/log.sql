7847878b1f4f156a16af078e47b40947ab34588e
kellytung2027 <144371984+kellytung2027@users.noreply.github.com>
2023-11-02 16:26:00 +0000

    Thu, Nov 2, 2023, 12:26 PM -04:00

--- pset_7/fiftyville/log.sql
+++ pset_7/fiftyville/log.sql
@@ -3,7 +3,7 @@
 -- find crime scene description
 SELECT description
 FROM crime_scene_reports
-WHERE month = 7 AND day = 28 AND street = 'Humphrey Street';
+WHERE year = 2023 AND month = 7 AND day = 28 AND street = 'Humphrey Street';
 -- info given: theft took place @ 10:15am at the Humphrey Street bakery, littering took place @ 16:36
 
 -- get transcripts from the date the robbery took place
