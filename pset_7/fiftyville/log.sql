986830f0e2daad6afa8410f75248ef8c07c8fbb3
kellytung2027 <144371984+kellytung2027@users.noreply.github.com>
2023-11-02 16:20:54 +0000

    Thu, Nov 2, 2023, 12:20 PM -04:00

--- pset_7/fiftyville/log.sql
+++ pset_7/fiftyville/log.sql
@@ -31,7 +31,7 @@ VALUES (
     SELECT name
     FROM people
     JOIN bakery_security_logs ON bakery_security_logs.license_plate = people.license_plate
-    WHERE month = 7 AND day = 28 AND hour = 10 AND (minute >= 5 OR minute <= 25);
+    WHERE month = 7 AND day = 28 AND hour = 10 AND (minute >= 5 OR minute <= 25)
 );
 
 
