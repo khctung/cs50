ed86ebaa98027d54a5a4cc2dc2824702af0be478
kellytung2027 <144371984+kellytung2027@users.noreply.github.com>
2023-11-02 04:22:50 +0000

    Thu, Nov 2, 2023, 12:22 AM -04:00

--- Classes/notes.py
+++ Classes/notes.py
@@ -203,3 +203,6 @@ AND name = 'Steve Carell';
 indexes = data structure that makes it faster to perform queries
 
 CREATE INDEX name ON table (column, ...)
+B-trees = every node has multiple children
+-> shorter tree = shorter leaves
+-> created in index
