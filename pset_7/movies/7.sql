dbfd11e91ce54b6bd78ca8377331312ac5417844
kellytung2027 <144371984+kellytung2027@users.noreply.github.com>
2023-11-02 06:59:17 +0000

    Thu, Nov 2, 2023, 2:59 AM -04:00

--- pset_7/movies/7.sql
+++ pset_7/movies/7.sql
@@ -1,12 +1,7 @@
 -- write a SQL query to list all movies released in 2010 and their ratings, in descending order by rating.
 -- For movies with the same rating, order them alphabetically by title.
-SELECT title, ratings
+SELECT title, rating
 FROM ratings
 JOIN movies ON ratings.movie_id = movies.id
-WHERE movies.year = 2010;
-ORDER BY rating DESC;
-
-
-
-Your query should output a table with two columns, one for the title of each movie and one for the rating of each movie.
-Movies that do not have ratings should not be included in the result.
+WHERE movies.year = 2010
+ORDER BY rating DESC, title ASC;
---
+++ pset_7/movies/8.sql
@@ -0,0 +1,5 @@
+-- write a SQL query to list the names of all people who starred in Toy Story.
+SELECT name
+
+Your query should output a table with a single column for the name of each person.
+You may assume that there is only one movie in the database with the title Toy Story.
