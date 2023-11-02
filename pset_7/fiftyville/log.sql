507a536eea378ab5b4e8c432a485d65976d36c08
kellytung2027 <144371984+kellytung2027@users.noreply.github.com>
2023-11-02 15:40:23 +0000

    Thu, Nov 2, 2023, 11:40 AM -04:00

--- pset_7/fiftyville/log.sql
+++ pset_7/fiftyville/log.sql
@@ -1,8 +1,13 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 
+-- find crime scene description
 SELECT description
 FROM crime_scene_reports
 WHERE month = 7 AND day = 28 AND street = 'Humphrey Street';
 -- info given: theft took place @ 10:15am at the Humphrey Street bakery, littering took place @ 16:36
 
 
+
+-- find possible suspects
+SELECT COUNT(name)
+FROM people;
