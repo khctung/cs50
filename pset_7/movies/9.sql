cda86415756b5f4f0e5f8d85c580fcaf990bf1e5
kellytung2027 <144371984+kellytung2027@users.noreply.github.com>
2023-11-02 07:45:45 +0000

    Thu, Nov 2, 2023, 3:45 AM -04:00

--- pset_7/movies/9.sql
+++ pset_7/movies/9.sql
@@ -1,7 +1,16 @@
 -- write a SQL query to list the names of all people who starred in a movie released in 2004, ordered by birth year.
 SELECT name
-FROM movie 
-ORDER BY birth
+FROM people
+JOIN movies ON people.id = movies.id
+WHERE movies.year = 2004
+ORDER BY people.birth ASC
+
+
+SELECT name
+FROM people
+JOIN stars ON people.id = stars.person_id
+JOIN movies ON stars.movie_id = movies.id
+WHERE movies.title = 'Toy Story';
 
 
 Your query should output a table with a single column for the name of each person.
