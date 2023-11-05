31fcd680292792d5cdd3db8d6c567954528477dd
kellytung2027 <144371984+kellytung2027@users.noreply.github.com>
2023-11-05 20:32:18 +0000

    Sun, Nov 5, 2023, 3:32 PM -05:00

--- pset_7/movies/6.sql
+++ pset_7/movies/6.sql
@@ -1,12 +1,5 @@
 -- write a SQL query to determine the average rating of all movies released in 2012.
 SELECT AVG(rating)
-from ratings
-WHERE movie_id
-IN (SELECT id FROM movies WHERE year = 2012);
-
--- OR; which is better design?
-
---SELECT AVG(rating)
---FROM ratings
---JOIN movies ON ratings.movie_id = movies.id
---WHERE movies.year = 2012;
+FROM ratings
+JOIN movies ON ratings.movie_id = movies.id
+WHERE movies.year = 2012;
