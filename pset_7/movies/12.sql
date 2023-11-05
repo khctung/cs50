ed06262d682b0c343c76d89a0ba470ce5948d7fb
kellytung2027 <144371984+kellytung2027@users.noreply.github.com>
2023-11-05 20:53:14 +0000

    Sun, Nov 5, 2023, 3:53 PM -05:00

--- pset_7/movies/12.sql
+++ pset_7/movies/12.sql
@@ -2,19 +2,15 @@
 SELECT title
 FROM movies
 WHERE id IN (
-  SELECT id
-  FROM people
-  WHERE name = 'Bradley Cooper'
+  SELECT movie_id
+  FROM stars
+  WHERE people.id = stars.person_id
+  AND stars.movie_id = movies.id
+  AND people.name = 'Bradley Cooper';
 ) AND id IN (
   SELECT movie_id
   FROM stars
-  JOIN people ON stars.person_id = people.id
-  WHERE name = 'Jennifer Lawrence'
+  WHERE people.id = stars.person_id
+  AND stars.movie_id = movies.id
+  AND people.name = 'Jennifer Lawrence';
 );
-
-WHERE person_id IN (
-  SELECT id
-  FROM people
-  WHERE name = 'Jennifer Lawrence'
-)
--- nested selects, replace join with a nested select
