cadead63c24055239ea07b97c4218a8cb90ffb59
kellytung2027 <144371984+kellytung2027@users.noreply.github.com>
2023-11-05 21:00:10 +0000

    Sun, Nov 5, 2023, 4:00 PM -05:00

--- pset_7/movies/13.sql
+++ pset_7/movies/13.sql
@@ -1,7 +1,7 @@
 -- write a SQL query to list the names of all people who starred in a movie in which Kevin Bacon also starred.
 SELECT DISTINCT name
 FROM people
-JOIN stars ON people.id = stars.person_id
+JOIN stars ON people.id = stars.person_id  -- was told if join then don't select
 WHERE movie_id IN (
   SELECT movie_id
   FROM stars
