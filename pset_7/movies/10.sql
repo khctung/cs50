a7955fb8c03b4534c1aaf3ef6321f82ad34f62a3
kellytung2027 <144371984+kellytung2027@users.noreply.github.com>
2023-11-05 21:25:33 +0000

    Sun, Nov 5, 2023, 4:25 PM -05:00

--- pset_7/movies/10.sql
+++ pset_7/movies/10.sql
@@ -1,8 +1,11 @@
 -- write a SQL query to list the names of all people who have directed a movie that received a rating of at least 9.0.
 SELECT DISTINCT name
 FROM people
-JOIN directors ON people.id = directors.person_id
-JOIN movies ON directors.movie_id = movies.id
-JOIN ratings ON movies.id = ratings.movie_id
-WHERE ratings.rating >= 9.0;
--- use nested select
+WHERE id IN (
+  SELECT person_id
+  FROM directors
+  WHERE movie_id IN (
+    SELECT movie_id
+    FROM ratings
+    WHERE rating >= 9.0)
+);
--- pset_7/movies/9.sql
+++ pset_7/movies/9.sql
@@ -1,8 +1,13 @@
 -- write a SQL query to list the names of all people who starred in a movie released in 2004, ordered by birth year.
 SELECT DISTINCT name
 FROM people
-JOIN stars ON people.id = stars.person_id
-JOIN movies ON stars.movie_id = movies.id
-WHERE movies.year = 2004
-ORDER BY people.birth ASC;
--- use nested selects
+WHERE id IN (
+  SELECT person_id
+  FROM stars
+  WHERE movie_id IN (
+    SELECT id
+    FROM movies
+    WHERE year = 2004
+  )
+)
+ORDER BY birth ASC;
