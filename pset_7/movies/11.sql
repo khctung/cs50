3171bf6e62a1290ba16d79b6efec36818610b1ea
kellytung2027 <144371984+kellytung2027@users.noreply.github.com>
2023-11-05 20:55:22 +0000

    Sun, Nov 5, 2023, 3:55 PM -05:00

--- pset_7/movies/11.sql
+++ pset_7/movies/11.sql
@@ -9,3 +9,13 @@ ORDER BY ratings.rating DESC
 LIMIT 5;
 -- def need to join movies and ratings (only getting one specific piece of info through nested select)
 -- WHERE for getting id for chadwick boseman
+
+
+WHERE id IN (
+  SELECT movie_id
+  FROM stars
+  WHERE person_id IN (
+    SELECT id
+    FROM people
+    WHERE name = 'Bradley Cooper')
+) 
