81522ed1124892e99cc16d32de0f919374aab4f5
kellytung2027 <144371984+kellytung2027@users.noreply.github.com>
2023-11-05 20:59:10 +0000

    Sun, Nov 5, 2023, 3:59 PM -05:00

--- pset_7/movies/13.sql
+++ pset_7/movies/13.sql
@@ -2,5 +2,12 @@
 SELECT DISTINCT name
 FROM people
 JOIN stars ON people.id = stars.person_id
-WHERE people.name = 'Kevin Bacon' AND people.birth = 1958
-AND people.name != 'Kevin Bacon';
+WHERE movie_id IN (
+  SELECT movie_id
+  FROM stars
+  WHERE person_id IN (
+    SELECT id
+    FROM people
+    WHERE name = 'Kevin Bacon' AND birth = 1958
+  )
+) AND name != 'Kevin Bacon';
