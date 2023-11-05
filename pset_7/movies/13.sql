3341a330d955f2e3cfafac3e78e7a8b0b5b7c047
kellytung2027 <144371984+kellytung2027@users.noreply.github.com>
2023-11-05 20:21:44 +0000

    Sun, Nov 5, 2023, 3:21 PM -05:00

--- pset_7/movies/13.sql
+++ pset_7/movies/13.sql
@@ -2,9 +2,7 @@
 SELECT DISTINCT name
 FROM people
 JOIN stars ON people.id = stars.person_id
-WHERE stars.movie_id IN (
-  SELECT stars.movie_id
-  FROM stars
-  JOIN people ON stars.person_id = people.id
-  WHERE people.name = 'Kevin Bacon' AND people.birth = 1958
-) AND people.name != 'Kevin Bacon';
+WHERE people.name = 'Kevin Bacon' AND people.birth = 1958
+AND people.name != 'Kevin Bacon';
+
+-- if join then no need for nested select (stars is already in my table)
